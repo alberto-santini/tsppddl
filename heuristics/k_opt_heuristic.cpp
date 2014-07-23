113801553f45b0a56ed898cc016397c6ba4f9210
alberto-santini <santini.alberto@gmail.com>
2014-07-23 08:53:51 +0200

    Corrected typo

--- heuristics/k_opt_heuristic.cpp
+++ heuristics/k_opt_heuristic.cpp
@@ -89,7 +89,7 @@ Path KOptHeuristic::get_path(const std::vector<std::vector<int>>& x) const {
     
     while(current_node != 2 * n + 1) {
         if(current_node == previous_node) {
-            sdt::cerr << "I got stuck!" << std::endl;
+            std::cerr << "I got stuck!" << std::endl;
             std::cerr << "X are:" << std::endl;
             for(int i = 0; i <= 2 * n - 1; i++) {
                 for(int j = 0; j <= 2 * n - 1; j++) {
