a358085193dedd1b97f599041879790c1b3af00f
Alberto Santini <santini.alberto@gmail.com>
2014-11-27 19:28:31 +0100

    Added cl parameter to specify config file

--- cli/program.cpp
+++ cli/program.cpp
@@ -18,20 +18,20 @@ void Program::load(const std::string& params_filename, const std::string& instan
 }
 
 void Program::autorun(const std::vector<std::string>& args) {
-    if(args.size() != 2) {
+    if(args.size() != 3) {
         print_usage();
         return;
     }
     
-    load("program_params.json", args[0]);
+    load(args[1], args[0]);
 
-    if(args[1] == "branch_and_cut") {
+    if(args[2] == "branch_and_cut") {
         auto hsolv = HeuristicSolver(*g, *params);
         heuristic_solutions = hsolv.solve();
         
         auto bsolv = BcSolver(*g, *params, heuristic_solutions, args[0]);
         bsolv.solve_with_branch_and_cut();
-    } else if(args[1] == "subgradient") {        
+    } else if(args[2] == "subgradient") {        
         auto hsolv = HeuristicSolver(*g, *params);
         heuristic_solutions = hsolv.solve();
         
@@ -44,8 +44,9 @@ void Program::autorun(const std::vector<std::string>& args) {
 
 void Program::print_usage() {
     std::cout << "Usage: " << std::endl;
-    std::cout << "\t./tsppddl <instance> [branch_and_cut | subgradient]" << std::endl;
+    std::cout << "\t./tsppddl <instance> <params> [branch_and_cut | subgradient]" << std::endl;
     std::cout << "\t\t instance: path to the json file with the instance data" << std::endl;
+    std::cout << "\t\t params: path to the json file with the program params" << std::endl;
     std::cout << "\t\t branch_and_cut: to start solving the problem with branch and cut" << std::endl;
     std::cout << "\t\t subgradient: to start solving the problem with lagrangian relaxation and the subgradient method" << std::endl;
 }
