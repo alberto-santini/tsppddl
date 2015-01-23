68313a778086f94853977fb00e949e61015b8e8d
Alberto Santini <santini.alberto@gmail.com>
2015-01-23 16:36:06 +0100

    Moved to CMake

---
+++ CMakeFiles/tsppddl.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tsppddl.dir/src/heuristics/heuristic.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/heuristics/k_opt_heuristic.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/network/graph_writer.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/network/path.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/network/tsp_graph.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/parser/parser.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/program/program.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/program/program_data.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/callbacks/cuts_callback.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/callbacks/cuts_lazy_constraint.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/callbacks/feasibility_cuts_separator.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/callbacks/print_relaxation_graph_callback.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/callbacks/vi_separator_capacity.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/callbacks/vi_separator_fork.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/callbacks/vi_separator_generalised_order.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/callbacks/vi_separator_simplified_fork.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/callbacks/vi_separator_subtour_elimination.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/bc_setup_model.raw.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/bc/bc_solver.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/heuristics/heuristic_solver.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/metaheuristics/tabu/kopt3_solver.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/metaheuristics/tabu/tabu_solver.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/subgradient/lagrange_setup_model.raw.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/solver/subgradient/subgradient_solver.cpp.o"
+  "CMakeFiles/tsppddl.dir/src/main.cpp.o"
+  "tsppddl.pdb"
+  "tsppddl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tsppddl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
