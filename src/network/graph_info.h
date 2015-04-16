eb02ad6ab7c875cf447b41da100af8add219815b
Alberto Santini <santini.alberto@gmail.com>
2015-04-16 08:22:16 +0200

    Changed k from int to double as it takes fractional values

--- src/network/graph_info.h
+++ src/network/graph_info.h
@@ -5,14 +5,14 @@
 
 struct graph_info {
     int n;
+    int h;
+    double k;
     int capacity;
     
     std::string instance_path;
     std::string instance_dir;
     std::string instance_name;
     std::string instance_base_name;
-    int h;
-    int k;
     
     graph_info() {}
     graph_info(int n, int capacity, std::string instance_path);
