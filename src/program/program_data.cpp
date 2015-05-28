b36ef52b44e3a90e202c5fde6e2b66e318b29b8a
Alberto Santini <santini.alberto@gmail.com>
2015-05-28 10:42:57 +0200

    Kept tabu search time, so it doesn't get set to 0

--- src/program/program_data.cpp
+++ src/program/program_data.cpp
@@ -6,6 +6,7 @@ void program_data::reset_times_and_cuts() {
     auto new_data = program_data();
     new_data.time_spent_by_constructive_heuristics = time_spent_by_constructive_heuristics;
     new_data.time_spent_by_k_opt_heuristics = time_spent_by_k_opt_heuristics;
+    new_data.time_spent_by_tabu_search = time_spent_by_tabu_search;
     
     std::swap(*this, new_data); 
 }
