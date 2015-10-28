8ba510f56f5e14fac9745ee66addc2423c1497fb
Alberto Santini <santini.alberto@gmail.com>
2015-10-28 19:36:30 +0100

    Changed instance generation and generated new instances

--- opt/data_translator.rb
+++ opt/data_translator.rb
@@ -61,11 +61,11 @@ def generate_instances(input_data)
   max_demand    = 99
   min_demand    = 1
   avg_demand    = ((min_demand + max_demand).to_f / 2).to_i
-  instance_name = File.basename(input_data[:original_file_name], ".dat").split("_").first
+  instance_name = File.basename(input_data[:original_file_name], ".tsp").split("_").first
   depot         = (0..(input_data[:num_ports]-1)).to_a.sample
   normal_ports  = (0..(input_data[:num_ports]-1)).to_a - [depot]
 
-  [8, 10, 12, 14, 16, 20, 24].each do |n|
+  [10, 14, 18, 22].each do |n|
     requests = Array.new
         
     0.upto(n-1) do |request|
@@ -79,9 +79,9 @@ def generate_instances(input_data)
       }
     end
 
-    [2, 3, 4, 5, n, 2*n].each do |h|
-      q                 = [h * avg_demand, requests.map{|r| r[:demand]}.max].max
-      possible_k_values = (h == 2*n ? [1.0] : [0.0, 0.25, 0.5, 0.75, 1.0])
+    [0.1, 0.3, 0.5, 2].each do |h|
+      q                 = ([h * n * avg_demand, requests.map{|r| r[:demand]}.max].max).to_i
+      possible_k_values = (h == 2 ? [1.0] : [0.0, 0.33, 0.67, 1.0])
       
       possible_k_values.each do |k|
         ports   = Array.new
@@ -100,8 +100,10 @@ def generate_instances(input_data)
               end
             end
     
-            draught[port] = max_port_demand + Random.rand(q - max_port_demand)
+            draught[port] = (max_port_demand + Random.rand(q - max_port_demand + 1)).to_i
           end
+          
+          draught[port] = (2 * n * max_demand).to_i if port == depot
   
           ports[port] = {
             :id       => port,
@@ -135,11 +137,11 @@ def read_process_and_print(original_file_name)
   
   instances.each do |instance|
     new_file_name = "../data/new/"
-    if instance[:h] == 2 * instance[:n]
-      new_file_name += "tsp_with_precedence/"
+    if instance[:h] == 2
+      new_file_name += "tsppd/"
     else
       if instance[:k] == 1.0
-        new_file_name += "tsppd/"
+        new_file_name += "ctsppd/"
       else
         new_file_name += "tsppddl/"
       end
